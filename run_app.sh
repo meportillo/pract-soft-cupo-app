6691bc318ad723024689ab04d01633de08829566
miguel <miguelequilea18@gmail.com>
2022-04-27 22:35:30 -0300

    agrego volumen al src para que se tomen los cambios en el momento

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t unque .
-docker run -p 3000:3000 --name unque_frontend -d unque
+docker run -p 3000:3000 --name unque_frontend -v $(pwd)/src:/usr/src/app/src -d unque
