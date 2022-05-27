882e29c629a30d89e1fe9e143db921afb435f07b
miguel <miguelequilea18@gmail.com>
2022-05-27 08:56:47 -0300

    refactor del formulario solicitud cupo

--- src/components/request/CreateRequest.js
+++ src/components/request/CreateRequest.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FcCancel } from "react-icons/fc";
-import { getSubjectsOfStudent } from '../../services/AlumnService';
+import { getSubjectsOfStudent, sendRequest } from '../../services/AlumnService';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -12,11 +12,10 @@ import { getUser } from "../../utils/auth";
 
 
 export default function CreateRequest(props){
-
     const [subjects,setSubjects] = useState([]);
     const [selected,setSelected] = useState("");
-    const [comisionSelected,setComisionSelected] = useState(""); 
     const [subjectsSelected,setSubjectsSelected] = useState([]);
+    const [comisiones,setComisiones] = useState([]);
     const [error,setError] = useState("");
     const navigate = useNavigate();
 
@@ -26,6 +25,10 @@ export default function CreateRequest(props){
         .then(data => {
             setSubjects(data);
         })
+        // getSubjects
+        // .then(data => {
+        //     setSubjects(data);
+        // })
     };
     const getComisiones = () => { 
         if (selected === ""){
@@ -37,22 +40,22 @@ export default function CreateRequest(props){
         }
     }
     const addSubject = () => {
-        if(selected === "" || comisionSelected === "") {
+        if(selected === "" || comisiones.length === 0) {
             setError("Falta seleccionar una materia o una comision"); 
         }
         else{
-            const sub = subjects.find(sub => sub.codigo == selected);
-            const repetidos = subjectsSelected.filter(s => s.codigo == sub.codigo); 
-            if (repetidos.length == 0) {
-                const comision = sub.comisiones.find(com => com.codComision == comisionSelected);
-                const obj = {"codigo":sub.codigo, "nombre":sub.nombre,"codComision":comisionSelected,"horario":`${comision.horaInicio}-${comision.horaFin}`}
-                const res =  subjectsSelected.slice();
-                res.push(obj);
-                setSubjectsSelected(res);
-            }
-            else{
-                setError(`La materia ${sub.nombre} ya se agrego`); 
-            }
+            // if (subjectsSelected.filter(sub => sub.codigo == selected ).length == 0) {
+                const subj = subjects.find(sub => sub.codigo == selected);
+                const coms = getComisiones(); 
+                const comsSelected = coms.filter((com,index) => comisiones.includes(`${index}`))
+                const obj = { "codigo":subj.codigo, "nombre": subj.nombre, "comisiones":comsSelected };
+                let clone = subjectsSelected.map((sub) =>{ return {...sub}}) 
+                clone.push(obj);
+                setSubjectsSelected(clone);
+            // }
+            // else{
+                // setError(`La materia ya se agrego`); 
+            // }
         } 
     }
     const deleteSubjectSelected = (codSubject,e) =>{
@@ -60,9 +63,40 @@ export default function CreateRequest(props){
         const res = subjectsSelected.filter(sub => sub.codigo != codSubject);
         setSubjectsSelected(res);
     }
+    const addCommission = (toAdd, indexComision) => {
+        if (toAdd) {
+            let copy = comisiones.slice();
+            copy.push(indexComision) 
+            setComisiones(copy)
+        }
+        else{
+            const copy = comisiones.filter(com => com != indexComision)
+            setComisiones(copy)        
+        }
+    }
+    const selectSubject = (idSubject) => {
+        document.querySelectorAll('input[type=checkbox]').forEach( el => el.checked = false );
+        if (selected != idSubject) {
+            setComisiones([]);
+            setSelected(idSubject)
+        }
+    }
+
     useEffect(getAllSubjects,[]);
+    
+    const sendForm = () => {
+        const user = JSON.parse(getUser());
+        sendRequest(subjectsSelected,user.dni)
+        .then(res => {
+            console.table(res)
+            navigate('/')
+        })
+        .catch(err => {
+            console.log(err)
+            setError(`ya se envio una solicitud de cupo`); 
+        })
+    }
     return( 
-        <div>
         <Form className="container">
             <h1></h1>
             {
@@ -79,7 +113,7 @@ export default function CreateRequest(props){
                     </Card.Header>
                     <Card.Body>
                         <Card.Title><Form.Label>Seleccionar Materia</Form.Label></Card.Title>
-                        <Form.Select id="selectMateria1" key="selectMateria" className="form-control" onChange={e => {setSelected(e.target.value)}}>
+                        <Form.Select id="selectMateria1" key="selectMateria" className="form-control" onChange={e => {selectSubject(e.target.value)}}>
                             <option key={0} value={""} >Seleccionar opcion</option>
                             {
                             subjects.map((sub,index) => <option key={index} value={sub.codigo}>{sub.nombre}</option>)
@@ -88,13 +122,21 @@ export default function CreateRequest(props){
                     </Card.Body>
                     <Card.Body>
                         <Card.Title><Form.Label>Seleccionar Comisión</Form.Label></Card.Title>
-                        <Form.Select id="selectComision1" key="selectComision" className="form-control" onChange={e => {setComisionSelected(e.target.value)}}>
-                            <option key={0} value={""} >Seleccionar opcion</option>
+                        <div>
                             {
-                            getComisiones().map((sub,index) => <option key={index} value={sub.codComision}>{sub.horaInicio}-{sub.horaFin}</option>)
+                            getComisiones().map((sub,index) =>  {
+                                let horarios = sub.horarios.map(c => `${c.dia} ${c.inicio}-${c.fin}`) 
+                                horarios = horarios.join() 
+                                return (<div key={index}>
+                                        <input type="checkbox" value={index} onChange={(e)=>addCommission(e.target.checked, e.target.value)} />
+                                        <span style={{"paddingLeft" : "10px"}}>{`Comision ${sub.comision}-${sub.modalidad} ${horarios}`}</span>
+                                        </div>)                              
+                            }     
+                            )
                             }
-                        </Form.Select>
-                        <br/>
+
+                        </div>
+                        <br></br>    
                         <Button variant="primary" onClick={addSubject}>Agregar Materia</Button>
                     </Card.Body>
                 </Card>
@@ -106,28 +148,29 @@ export default function CreateRequest(props){
                             <tr key={0}>
                             <th>Cancelar</th>
                             <th>Materia</th>
-                            <th>Comision</th>
-                            <th>Horario</th>
+                            <th>Codigo</th>
+                            <th>comisiones</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                subjectsSelected.map((matSelect,index) => 
-                                    <tr key={index}>
-                                        <td>{<button onClick={ e => deleteSubjectSelected(matSelect.codigo,e)}><FcCancel  /></button>}</td>
-                                        <td>{matSelect.nombre}</td>
-                                        <td>{matSelect.codComision}</td>
-                                        <td>{matSelect.horario}</td>
-                                    </tr>    
+                                subjectsSelected.map((matSelect,index) =>{
+                                    const comsMat = matSelect.comisiones.map(m => `comision ${m.comision}`).join()  
+                                    return (<tr key={index}>
+                                            <td>{<button onClick={ e => deleteSubjectSelected(matSelect.codigo,e)}><FcCancel  /></button>}</td>
+                                            <td>{matSelect.nombre}</td>
+                                            <td>{matSelect.codigo}</td>
+                                            <td>{comsMat}</td>
+                                            </tr>)    
+                                } 
                                 )
                             }
                         </tbody>
                     </Table>
                 </Form.Label>
-                    <Button onClick={e => { navigate('/')}} className="col align-self-end btn btn-primary">Generar Solicitud</Button>                  
+                    <Button onClick={sendForm} className="col align-self-end btn btn-primary">Generar Solicitud</Button>                  
             </Form.Group>
         </Form>
-        </div>
     )
 
 }
